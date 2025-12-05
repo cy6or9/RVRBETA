f5c43d414eb90184cacb37e0fa8be2b281dfdad2
Cy6or9 <cy6or9@yahoo.com>
2025-12-05 04:15:56 +0000

    adminn pages adjustments

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverActions: false, // <-- REQUIRED FOR NETLIFY API ROUTES
+  },
 };
 
 module.exports = nextConfig;
