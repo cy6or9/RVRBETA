b533e3b3f4e2bc40151eead070b7c2fbf5567cfe
Cy6or9 <cy6or9@yahoo.com>
2025-12-28 07:37:47 +0000

    logging in fixes

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -93,7 +93,7 @@ export function AuthProvider({ children }) {
     });
 
     return () => unsub();
-  }, []);
+  }, [user]);
 
   // Handle page close/hide: save session duration
   useEffect(() => {
